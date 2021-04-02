48dafe3d34e0c34af524a0ec9cf41a4ee96ebd21
kiteB <mmy789@sch.ac.kr>
2021-04-02 16:07:38 +0900

    코루틴

---
+++ Concurrency_Socket_programming2/sync_get_user.py
@@ -0,0 +1,20 @@
+# 동기 처리 프로그램
+import time
+
+
+def get_user(name):
+    print('사용자 {!r} 정보 조회중...'.format(name))
+    time.sleep(1)
+    print('사용자 {!r} 정보 조회 완료!'.format(name))
+
+
+def main():
+    start = time.time()
+    get_user('Jeon')
+    get_user('Yun')
+    get_user('Kim')
+    get_user('Song')
+    end = time.time()
+    print(f"총 소요시간: {end - start}")
+
+main()
