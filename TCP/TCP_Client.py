0eea25f86a789f0927518f13c0271d22b5539da1
kiteB <mmy789@sch.ac.kr>
2021-04-14 23:04:29 +0900

    TCP

---
+++ TCP/TCP_Client.py
@@ -0,0 +1,9 @@
+# TCP 클라이언트 예제
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+addr = ('localhost', 9000)
+sock.connect(addr)
+msg = sock.recv(1024)
+print(msg.decode())
+sock.close()
