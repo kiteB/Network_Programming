02af182510a2032ddd3d1c1641a72f48292bc5cd
kiteB <mmy789@sch.ac.kr>
2021-04-22 23:16:00 +0900

    멀티스레드 에코 서버

---
+++ Concurrency_Socket_programming/thread_class_echo_server.py
@@ -0,0 +1,31 @@
+# 멀티스레드 에코 서버: Thread의 파생 클래스 이용
+from socket import *
+import threading
+
+port = 2500
+BUFSIZE = 1024
+
+class ClientThread(threading.Thread):
+    def __init__(self, sock):
+        threading.Thread.__init__(self)
+        self.sock = sock
+
+    def run(self):
+        while True:
+            data = self.sock.recv(BUFSIZE)
+            if not data:
+                break
+            print('Received message:', data.decode())
+            self.sock.send(data)
+
+        self.sock.close()
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind(('', port))
+sock.listen(5)
+
+while True:
+    conn, (remotehost, remoteport) = sock.accept()
+    print('connected by', remotehost, remotehost)
+    th = ClientThread(conn)
+    th.start()
