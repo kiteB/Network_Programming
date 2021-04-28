645cd83820aaacc8ac44822a1f8d0b37b6de45c8
kiteB <mmy789@sch.ac.kr>
2021-04-28 20:06:00 +0900

    I/O-bound 프로세스 성능 향상 방안

---
+++ Concurrency_Socket_programming2/simple_requests.py
@@ -0,0 +1,27 @@
+# 기본 프로그램 (동시성 없음)
+# - 2개의 사이트를 번갈아 가면서 총 160번의 웹 페이지 요청/응답 수신
+# - 1개의 요청/응답 수신 후, 다음 요청/응답 수신을 수행함
+import requests
+import time
+
+
+def download_site(url,session):
+    with session.get(url) as response:
+        print(f"Read {len(response.content)} from {url}")
+
+
+def download_all_sites(sites):
+    with requests.Session() as session:
+        for url in sites:
+            download_site(url, session)
+
+
+if __name__ == "__main__":
+    sites = [
+                "https://homepage.sch.ac.kr",
+                "https://www.google.co.kr",
+            ] * 80
+    start_time = time.time()
+    download_all_sites(sites)
+    duration = time.time() - start_time
+    print(f"Downloaded {len(sites)} in {duration} seconds")
